d9d89ea164594315907a20223af53f0cbe2311e8
Aaron Thompson <thompson_ad@outlook.com>
2020-03-28 22:48:13 +0000

    first mutation

--- src/server/services/graphql/resolvers.js
+++ src/server/services/graphql/resolvers.js
@@ -22,6 +22,21 @@ const resolvers = {
     posts(root, args, context) {
       return posts;
     }
+  },
+
+  // the resolver extracts the post and user objects from the mutations parameters which are passed in the second argument of the function
+  // Then, we build the postObject variable
+  // the postObject variable now looks like a post from the fake data now
+  RootMutation: {
+    addPost(root, { post, user }, context) {
+      const postObject = {
+        ...post,
+        user,
+        id: posts.length + 1
+      };
+      posts.push(postObject);
+      return postObject;
+    }
   }
 };
 
