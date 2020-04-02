ba49212273c10cd2d0a3b78f3588cf61e275e00d
Aaron Thompson <thompson_ad@outlook.com>
2020-04-02 08:29:50 +0100

    adding apollo provider

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
 import App from "./App";
 import client from "./apollo";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// every component now has access to the apollo client
+// Apollo client gives us everything we need to be able to send requests from our react components
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
