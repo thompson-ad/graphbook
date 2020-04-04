517b66be0157cb2789cf3d5be63de13750bfe197
Aaron Thompson <thompson_ad@outlook.com>
2020-04-04 22:26:18 +0100

    hook up chats query

--- src/client/Chats.js
+++ src/client/Chats.js
@@ -1,42 +1,22 @@
 import React, { Component } from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
 
-// get some fake data to play with from Apollo client
-const chats = [
+const GET_CHATS = gql`
   {
-    id: 1,
-    users: [
-      {
-        avatar: "/uploads/avatar1.png",
-        username: "TestUser",
-      },
-      {
-        avatar: "/uploads/avatar2.png",
-        username: "TestUser2",
-      },
-    ],
-    lastMessage: {
-      id: 4,
-      text: "you just added a message",
-    },
-  },
-  {
-    id: 2,
-    users: [
-      {
-        avatar: "/uploads/avatar1.png",
-        username: "TestUser",
-      },
-      {
-        avatar: "/uploads/avatar2.png",
-        username: "TestUser2",
-      },
-    ],
-    lastMessage: {
-      id: 4,
-      text: "you just added a message",
-    },
-  },
-];
+    chats {
+      id
+      users {
+        id
+        avatar
+        username
+      }
+      lastMessage {
+        text
+      }
+    }
+  }
+`;
 
 export default class Chats extends Component {
   usernamesToString(users) {
@@ -64,23 +44,31 @@ export default class Chats extends Component {
     // we map over all of the chats and return a new list item for each chat
     return (
       <div className="chats">
-        {chats.map((chat, i) => (
-          <div className="chat">
-            <div className="header">
-              <img
-                src={
-                  chat.users.length > 2
-                    ? "/public/group.png"
-                    : chat.users[1].avatar
-                }
-              />
-              <div>
-                <h2>{this.shorten(this.usernamesToString(chat.users))}</h2>
-                <span>{this.shorten(chat.lastMessage.text)}</span>
+        <Query query={GET_CHATS}>
+          {({ loading, error, data }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return error.message;
+
+            const { chats } = data;
+            return chats.map((chat, i) => (
+              <div className="chat">
+                <div className="header">
+                  <img
+                    src={
+                      chat.users.length > 2
+                        ? "/public/group.png"
+                        : chat.users[1].avatar
+                    }
+                  />
+                  <div>
+                    <h2>{this.shorten(this.usernamesToString(chat.users))}</h2>
+                    <span>{this.shorten(chat.lastMessage.text)}</span>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        ))}
+            ));
+          }}
+        </Query>
       </div>
     );
   }
